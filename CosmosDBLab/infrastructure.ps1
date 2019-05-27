438a5ca504c31d6b955fa346603bc8f5a5c78d60
Code$izzler <30734357+CodeSizzler@users.noreply.github.com>
2019-05-27 12:39:57 +0530

    Add files via upload

---
+++ CosmosDBLab/infrastructure.ps1
@@ -0,0 +1,31 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco feature enable -n allowGlobalConfirmation
+
+choco install vscode
+
+choco install git
+
+choco install dotnetcore-sdk
+
+choco install dotnet4.5.1
+
+choco install jdk8
+
+choco install jre8
+
+choco install maven
+
+choco install googlechrome -force
+
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+code --install-extension vscjava.vscode-java-pack
